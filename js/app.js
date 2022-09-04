edf4e3a3d40b3f5998ff97001bcdc3c416486b3f
Sheikh sadi <you@example.com>
2022-09-04 11:20:21 -0700

    blog page added

--- js/app.js
+++ js/app.js
@@ -26,14 +26,14 @@ const displayNews = (allNews) =>{
     newsContainer.innerHTML = ``;
     allNews.forEach(news =>{
         
-        console.log(news);
+        // console.log(news);
         const newsDiv = document.createElement('div');
         newsDiv.innerHTML = `
-        <div class="row g-0 p-3 my-2 bg-white rounded">
-        <div class="col-12  col-md-3 "><img class="img-fluid headline-image h-100 w-100" src="${news.thumbnail_url}" alt=""></div>
-        <div class="col-md-9 p-3">
+        <div class="row g-0 p-3 my-3 bg-white rounded ">
+        <div class="col-12  col-md-3 "><img class="img-fluid headline-image h-100 w-100" src="${news.thumbnail_url ? news.thumbnail_url : 'No thumbnail found'}" alt=""></div>
+        <div class="col-md-9 p-4">
           <div>
-            <h3 class="fw-bold">${news.title}</h3>
+            <h3 class="fw-bold">${news.title ? news.title : "No title found"}</h3>
             <p class="py-1 text-muted">${news.details.slice(0,300)}...</p>
           </div>
           <div class="d-flex justify-content-between align-items-center flex-wrap">
@@ -54,10 +54,11 @@ const displayNews = (allNews) =>{
               <div>Ratings: ${news.rating.number}</div>
             </div>
   
-            <div class="mx-2"><i class="fa-solid fa-circle-right text-primary"></i></div>
+            <div onclick="loadModal('${news._id}')" class="mx-2" role="button" data-bs-toggle="modal" data-bs-target="#newsDetailModal"><i class="fa-solid fa-circle-right text-primary"></i></div>
           </div>
         </div>
       </div>
+
         `;
         newsContainer.appendChild(newsDiv);
        
@@ -78,6 +79,19 @@ const toggleSpinner = isLoading =>{
     else{
         loaderSection.classList.add('d-none')
     }
+}
+// modal data load 
+const loadModal = (modalId) =>{
+    fetch(`https://openapi.programming-hero.com/api/news/${modalId}`)
+    .then(res => res.json())
+    .then(data => displayModalDetail(data.data[0]))
+}
+const displayModalDetail = (modalNews) =>{
+    console.log(modalNews);
+    const modalTitle = document.getElementById('newsDetailModalLabel');
+    modalTitle.innerText = modalNews.title;
+    const modalDetails = document.getElementById('news-detail');
+    modalDetails.innerText = modalNews.details;
 }
 
 loadNews('01');
